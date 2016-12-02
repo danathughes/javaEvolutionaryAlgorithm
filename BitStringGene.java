209b5073325dd520bd37245a811d7ec9b10f370b
Dana Hughes <danathughes@gmail.com>
2016-12-01 20:49:49 -0700

    Renamed BitStringGenotype as BitStringGene

---
+++ BitStringGene.java
@@ -0,0 +1,18 @@
+/**
+*
+*
+* @author	Dana Hughes
+* @version	1.0
+* @since	2016-11-30
+*/
+
+
+public class BitStringGenotype extends Genotype
+{
+	private int stringLength;
+
+	public BitStringGenotype(int stringLength)
+	{
+		this.stringLength = stringLength;
+	}
+}
