83f0eba80c0bfcc8b8d20ae7d3929b895b9b19c2
Dana Hughes <danathughes@gmail.com>
2016-12-12 19:25:39 -0700

    Renamed Gene classes to Genoytpe classes

---
+++ src/placeholder/EvolutionaryProgramming/base/Genotype.java
@@ -0,0 +1,21 @@
+/**
+*
+*
+* @author	Dana Hughes
+* @version	1.0
+* @since	2016-11-30
+*/
+
+package placeholder.EvolutionaryProgramming.base;
+
+import java.util.Random;
+
+public abstract class Genotype
+{
+	public abstract void randomize();
+
+	public abstract Genotype[] crossover(Genotype otherGene, double crossoverRate);
+	public abstract void mutate(double mutationRate);
+
+	public abstract String toString();
+}
